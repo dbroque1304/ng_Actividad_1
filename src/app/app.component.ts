705e274df76ec39557fe014a6d686bca126baf4d
dbroque1304 <dbroque1304@g.educaand.es>
2024-01-15 12:41:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './componentes/contador/contador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componentes/contador/contador.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  imports: [],
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+ i:number = 0;
+ contar(){
+  this.i++;
+ }
+}
